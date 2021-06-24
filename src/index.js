1c1f46ea68b0a3a18ff561fc4d9cd40420263323
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-24 14:01:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import '../assets/application.css';
 
 //import gon from 'gon';
 
-//import app from './app';
+import app from './app';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-//app(gon);
+app();
