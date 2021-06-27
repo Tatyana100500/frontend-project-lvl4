86db987d7e257c6d69e06d9180321711e62c6901
Tatiana Chernishova <chernishovate@mail.ru>
2021-06-27 15:43:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import '../assets/applications.scss';
 import i18nInit from './i18nInit';
 import renderApp from './init';
 
-console.log('gon', renderApp);
-console.log('i18nInit', i18nInit);
-i18nInit().then(() => renderApp(window.gon));
+i18nInit().then(() => {
+console.log(renderApp(window.gon));
+renderApp(window.gon)});
