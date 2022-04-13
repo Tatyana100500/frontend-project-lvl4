4aaca95ba675d1babbec78089cc59affb2453fdf
Tatiana Chernishova <chernishovate@mail.ru>
2022-04-13 17:10:23 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const render = async () => {
+export default render = async () => {
   const vdom = await init();
 
   ReactDOM.render(vdom, document.getElementById('chat'));
